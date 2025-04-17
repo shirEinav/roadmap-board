cb56720cca75c52f36255281e00ef769433e245f
Shir Einav <shire@wix.com>
2025-04-17 22:28:02 +0300

    fix config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 export const App = () => {
   return (
-    <></>
+    <>Roadmap Board</>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite"
-import * as react from "@vitejs/plugin-react"
+import react from "@vitejs/plugin-react"
 import { resolve } from "path"
 
 // https://vitejs.dev/config/
